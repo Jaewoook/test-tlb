92002c165a930bc046e15ab4507e02aa3296a005
Jaewook Ahn <ajw4586@gmail.com>
2022-04-17 22:07:55 +0900

    Improve test script able to set stride
    - default stride 64

--- test-random-with-output.sh
+++ test-random-with-output.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 OUTPUT_FILENAME=result-$(TZ='Asia/Seoul' date -Iminutes).csv
+STRIDE=$1
 
-echo "Starting test-tlb"
+if [ $# -eq 0 ]
+then
+    STRIDE=64
+fi
+
+echo "Starting test-tlb with stride=$STRIDE"
 echo ""
 
 echo "Size,Access Time,Cycles" >> $OUTPUT_FILENAME
 for i in 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 6M 8M 16M 32M 64M 128M 256M
 do
-    echo "./test-tlb -r $i 64"
+    echo "./test-tlb -r $i $STRIDE"
     echo -n "$i," >> $OUTPUT_FILENAME
-    ./test-tlb -r $i 64 >> $OUTPUT_FILENAME
+    ./test-tlb -r $i $STRIDE >> $OUTPUT_FILENAME
 done

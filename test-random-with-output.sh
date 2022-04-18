1f65b416733b444a26fd874ec8fac24c5eeffdb4
Jaewook Ahn <ajw4586@gmail.com>
2022-04-18 20:28:42 +0900

    Improve test script for various strides

--- test-random-with-output.sh
+++ test-random-with-output.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-OUTPUT_FILENAME=result-$(TZ='Asia/Seoul' date -Iminutes).csv
 STRIDE=$1
+MAP="4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 6M 8M 16M 32M 64M 128M 256M"
 
 if [ $# -eq 0 ]
 then
-    STRIDE=64
+    STRIDE="4 8 16 32 64 128 256 512 1024 2048 4096 8192 10240 16384 32768 65536"
 fi
 
-echo "Starting test-tlb with stride=$STRIDE"
-echo ""
 
-echo "Size,Access Time,Cycles" >> $OUTPUT_FILENAME
-for i in 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 6M 8M 16M 32M 64M 128M 256M
+echo "Starting test-tlb"
+echo "Target map sizes: $MAP"
+echo "Target strides: $STRIDE"
+
+for stride in $STRIDE
 do
-    echo "./test-tlb -r $i $STRIDE"
-    echo -n "$i," >> $OUTPUT_FILENAME
-    ./test-tlb -r $i $STRIDE >> $OUTPUT_FILENAME
+    OUTPUT_FILENAME="result-stride-$stride.csv"
+    echo -e "\nstride: $stride"
+    echo "Size,Access Time,Cycles" >> $OUTPUT_FILENAME
+
+    for map in $MAP
+    do
+        echo -n "$map," >> $OUTPUT_FILENAME
+        echo "./test-tlb -r $map $stride"
+        ./test-tlb -r $map $stride >> $OUTPUT_FILENAME
+    done
+
+    echo -e "\nInfo: MAP=$MAP STRIDE=$STRIDE" >> $OUTPUT_FILENAME
 done
+
+echo "Done"
